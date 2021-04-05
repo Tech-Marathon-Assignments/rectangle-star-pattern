2e384bff2f27bc8cd1c87a54a70940e5af0e474f
duy.leduc8 <duy.leduc8@gmail.com>
2021-04-05 22:02:40 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+"""
+Bài 4: Viết chương trình vẽ hình chữ nhật bằng dấu sao ( “*” )
+(Số hàng và số cột phải là số dương lớn hơn 0)
+Exercise 4: Write a program to print rectangle star pattern.
+(The number of rows and columns must be a positive number greater than 0)
+
+Sample input: row = 3, column = 4
+Sample output: ****
+****
+****
+"""
+
+
+def main(rows, cols):
+    pass
+
+
+main(3, 4)
